fc4d35d1123794847dd1c317f2d81ab45f30cbff
Lloyd <lloyd.madiba@gmail.com>
2023-03-22 15:12:49 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _isalpha - Checks for alphabetic character
+ * print_sign - Prints the sign of a number
  * @c: The number to be checked
- * Return: 1 for positive num, -1 for negative number or zero for anything else
+ * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 int print_sign(int n)
 {
+
 	if (n > 0)
 	{
 	_putchar(43);
