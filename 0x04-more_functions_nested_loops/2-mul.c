e300e2e946fe798181ef292d2e5d37a42b9f0f86
Lloyd <lloyd.madiba@gmail.com>
2023-03-24 13:45:53 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,12 @@
 
 /**
  * mul - multiplies two integers
- * @c: The first integer
- * @b: The second integer
+ * @c: - The first integer
+ * @b: - The second integer
  * Return: Always 0
  */
 
 int mul(int a, int b)
-
 {
 int result = a * b;
 return (result);
