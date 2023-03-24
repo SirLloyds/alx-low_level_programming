2c5ab207ba5c011bde607bcd80366fdb090e75ee
Lloyd <lloyd.madiba@gmail.com>
2023-03-24 13:39:08 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @c: The first integer
+ * @b: The second integer
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+
+{
+int result = a * b;
+return (result);
+}
