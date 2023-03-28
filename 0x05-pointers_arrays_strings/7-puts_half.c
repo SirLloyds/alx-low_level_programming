4063793864e458507fa03775cf7ec33c501a818f
Lloyd <lloyd.madiba@gmail.com>
2023-03-28 03:23:23 -0700

    ./7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,14 +9,14 @@ void puts_half(char *str)
 	int j = 0;
 	int k;
 
-	while (str[j]) != '\0')
+	while (str[j] != '\0')
 	{
 		j++;
 	}
 	if (j % 2 == 1)
 	{
 		k = (j - 1) / 2;
-		k += 1
+		k += 1;
 	}
 	else
 	{
