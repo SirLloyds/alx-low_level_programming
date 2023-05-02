fcc7948b509f1ff0960d17363626ef63c42b9355
Lloyd <lloyd.madiba@gmail.com>
2023-05-02 13:41:38 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - function that frees list
+ * @head: double head of a list
+ *
+ * Return: Void
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *next;
+
+if (head == NULL)
+return;
+while (*head != NULL)
+{
+next = (*head)->next;
+free(*head);
+*head = next;
+}
+}
